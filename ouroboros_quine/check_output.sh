aca99f2ba69000b4781b6707623962ed7a753d83
rumia-san <rumia_zhihu@hotmail.com>
2020-09-22 21:41:51 +0800

    Modify C++ code to C code

--- ouroboros_quine/check_output.sh
+++ ouroboros_quine/check_output.sh
@@ -2,22 +2,22 @@
 
 mkdir -p output
 
-gcc quine.cpp -Wall -std=c++11 -o ./quine_cpp
-./quine_cpp > ./output/quine.py
+gcc quine.c -Wall -o ./quine_c
+./quine_c > ./output/quine.py
 if [[ $(diff ./quine.py ./output/quine.py) ]]; then
-    echo "ERROR! Output of quine.cpp is different from quine.py!"
+    echo "ERROR! Output of quine.c is different from quine.py!"
 else
-    echo "GREAT! Output of quine.cpp is the same as quine.py!"
+    echo "GREAT! Output of quine.c is the same as quine.py!"
 fi
 
-python ./quine.py > ./output/quine.cpp
-if [[ $(diff ./quine.cpp ./output/quine.cpp) ]]; then
-    echo "ERROR! Output of quine.py is different from quine.cpp!"
+python ./quine.py > ./output/quine.c
+if [[ $(diff ./quine.c ./output/quine.c) ]]; then
+    echo "ERROR! Output of quine.py is different from quine.c!"
 else
-    echo "GREAT! Output of quine.py is the same as quine.cpp!"
+    echo "GREAT! Output of quine.py is the same as quine.c!"
 fi
 
 echo "Here are the sha1 sums:"
-sha1sum ./quine.cpp ./output/quine.cpp
+sha1sum ./quine.c ./output/quine.c
 sha1sum ./quine.py ./output/quine.py
 
--- ouroboros_quine/quine.c
+++ ouroboros_quine/quine.c
@@ -1,42 +1,42 @@
-#include<cstdio>
+#include<stdio.h>
 int main(void) {
     const char* python_code[] = {
-        "cpp_code = [",
+        "c_code = [",
         "        %c%s%c,",
         "]",
         "python_code = [",
         "        %c%s%c,",
         "]",
         "for i in range(3):",
-        "    print(cpp_code[i])",
+        "    print(c_code[i])",
         "for code in python_code:",
         "    print(python_code[1] % (0x22, code, 0x22))",
         "for i in range(3, 5):",
-        "    print(cpp_code[i])",
-        "for code in cpp_code:",
+        "    print(c_code[i])",
+        "for code in c_code:",
         "    print(python_code[1] % (0x22, code, 0x22))",
         "for i in range(5, 13):",
-        "    print(cpp_code[i])",
+        "    print(c_code[i])",
     };
-    const char* cpp_code[] = {
-        "#include<cstdio>",
+    const char* c_code[] = {
+        "#include<stdio.h>",
         "int main(void) {",
         "    const char* python_code[] = {",
         "    };",
-        "    const char* cpp_code[] = {",
+        "    const char* c_code[] = {",
         "    };",
         "    puts(python_code[0]);",
-        "    for (auto code : cpp_code) { printf(python_code[1], 0x27, code, 0x27); putchar(0x0A); }",
+        "    for (int i = 0; i < 13; i++) { printf(python_code[1], 0x27, c_code[i], 0x27); putchar(0x0A); }",
         "    puts(python_code[2]); puts(python_code[3]);",
-        "    for (auto code : python_code) { printf(python_code[4], 0x27, code, 0x27); putchar(0x0A); }",
+        "    for (int i = 0; i < 16; i++) { printf(python_code[4], 0x27, python_code[i], 0x27); putchar(0x0A); }",
         "    for (int i = 5; i < 16; i++) puts(python_code[i]);",
         "    return 0;",
         "}",
     };
     puts(python_code[0]);
-    for (auto code : cpp_code) { printf(python_code[1], 0x27, code, 0x27); putchar(0x0A); }
+    for (int i = 0; i < 13; i++) { printf(python_code[1], 0x27, c_code[i], 0x27); putchar(0x0A); }
     puts(python_code[2]); puts(python_code[3]);
-    for (auto code : python_code) { printf(python_code[4], 0x27, code, 0x27); putchar(0x0A); }
+    for (int i = 0; i < 16; i++) { printf(python_code[4], 0x27, python_code[i], 0x27); putchar(0x0A); }
     for (int i = 5; i < 16; i++) puts(python_code[i]);
     return 0;
 }
